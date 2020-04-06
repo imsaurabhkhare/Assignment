1b1cc5c00d5d2008484c680558b5073ce3dcb699
imsaurabhkhare <imsaurabhkhare@gmail.com>
2020-04-06 17:59:13 +0530

    Guessing game

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,42 @@
 function guess {
 local count=0
-for code in $(ls)
+for code in $(ls -a)
 do
 let count=$count+1
 done
 echo $count
 }
-echo "how many file in a directory "
+
+function input {
 read response
+if [[ $response =~ [A-Za-z] ]]
+then
+echo "incorrect input"
+else
+echo $response
+fi
+}
+
+echo "how many file in a directory "
+response=$(input)
+while [[ $response = "incorrect input" ]]
+do
+echo "incorrect input"
+response=$(input)
+done
 while [[ $response -ne $(guess) ]]
 do
 if [[ $response -lt $(guess) ]]
 then
 echo "guess was to low"
-read response
 else
 echo "guess was to high"
-read response
 fi
+response=$(input)
+while [[ $response = "incorrect input" ]]
+do
+echo "incorrect input"
+response=$(input)
+done
 done
 echo "correct guess"
